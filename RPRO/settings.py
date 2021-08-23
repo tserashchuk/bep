d7cd8373573bc69be2acddbe645e675d93e3c16d
tserashchuk <tserashchuk.kirill@gmail.com>
2021-08-23 15:50:40 +0000

    first prod

--- RPRO/settings.py
+++ RPRO/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!5i9)j-2y^2i@m&x66u#)#3+++@01)bd&-jhm)!fvu-+f!3lks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rpro.pythonanywhere.com','127.0.0.1:8000','127.0.0.1']
 
